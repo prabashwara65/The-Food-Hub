8fbcdb823f69842102dec2d24009a872f69ce012
prabashwara65 <prabashwara65@gmail.com>
2025-04-13 09:04:41 +0530

    creating home component

--- Frontend/Pages/Prabashwara/Home.jsx
+++ Frontend/Pages/Prabashwara/Home.jsx
@@ -6,13 +6,12 @@ import { clearUser } from '../../ReduxToolKit/userSlice';
 const Home = () => {
   const user = useSelector((state) => state.user.user)
   return (
-    <div>
-      <h1 className="text-xl">Home</h1>
-      <div className="bg-red-500  p-4">{user.email}</div>
-      <h1>Home must have SerchBard</h1>
-      <h1>Home must have Hero Section</h1>
-      <h1>Home must have Grid View</h1>
-      <h1>Home must have footer</h1>
+    <div className="h-screen bg-[#F6F6FF]">
+
+      {/* Search bar */}
+      <div className="container items-center justify-center  bg-amber-800">
+        <div>sfs</div>
+      </div>
     </div>
   );
 };
