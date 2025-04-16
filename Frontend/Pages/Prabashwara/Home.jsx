1dca2a0ff5f4ac819444b2e018239b73525e7b8e
prabashwara65 <prabashwara65@gmail.com>
2025-04-16 16:44:32 +0530

    home was designing

--- Frontend/Pages/Prabashwara/Home.jsx
+++ Frontend/Pages/Prabashwara/Home.jsx
@@ -34,7 +34,6 @@ const Home = () => {
 
           {/* +Add order Button */}
           <button className="border-2 ml-8 rounded-3xl p-4 text-white bg-[#B97A9E]">
-            {" "}
             + Add Order{" "}
           </button>
         </div>
@@ -49,8 +48,138 @@ const Home = () => {
               explicabo illum quidem, repellat fugiat accusamus! Vero quia
               nostrum consectetur aspernatur deleniti!
             </p>
-            <button className="rounded-4xl p-4 uppercase bg-[#745200] text-white w-[200px]">Read More</button>
+            <button className="rounded-4xl p-4 uppercase bg-[#745200] text-white w-[200px]">
+              Read More
+            </button>
           </div>
+        </div>
+
+        <div className="container mx-auto flex flex-row mt-25 gap-7">
+        <div className="relative flex flex-1/4 h-50 bg-[#FFFFFF] rounded-3xl">
+            <div className="absolute top-[-50px] left-23 w-30 h-30 rounded-full bg-red-200"></div>
+              <div className="absolute w-30 h-38 bottom-0 p-4 pt-8">
+                {/* Rating */}
+                <div className="flex">
+                  <svg
+                    xmlns="http://www.w3.org/2000/svg"
+                    fill="none"
+                    viewBox="0 0 24 24"
+                    strokeWidth={1.5}
+                    stroke="currentColor"
+                    className="size-5 pt-1 fill-[#AE859B] text-[#AE859B]"
+                  >
+                    <path
+                      strokeLinecap="round"
+                      strokeLinejoin="round"
+                      d="M11.48 3.499a.562.562 0 0 1 1.04 0l2.125 5.111a.563.563 0 0 0 .475.345l5.518.442c.499.04.701.663.321.988l-4.204 3.602a.563.563 0 0 0-.182.557l1.285 5.385a.562.562 0 0 1-.84.61l-4.725-2.885a.562.562 0 0 0-.586 0L6.982 20.54a.562.562 0 0 1-.84-.61l1.285-5.386a.562.562 0 0 0-.182-.557l-4.204-3.602a.562.562 0 0 1 .321-.988l5.518-.442a.563.563 0 0 0 .475-.345L11.48 3.5Z"
+                    />
+                  </svg>
+                  <p className="text-md pl-1 text-[#AE859B]">4.7</p>
+                </div>
+
+                <p className="w-2xs pt-4 pl-1">
+                  Special <span className="block">Chiken rice Bowl</span>{" "}
+                </p>
+                {/* Price */}
+              </div>
+              <div className="absolute flex justify-center items-center w-11 h-11 bottom-8 right-5 bg-[#AE859B] rounded-full text-sm text-white font-medium">
+                250
+              </div>
+          </div>
+          <div className="relative flex flex-1/4 h-50 bg-[#FFFFFF] rounded-3xl">
+            <div className="absolute top-[-50px] left-23 w-30 h-30 rounded-full bg-red-200"></div>
+              <div className="absolute w-30 h-38 bottom-0 p-4 pt-8">
+                {/* Rating */}
+                <div className="flex">
+                  <svg
+                    xmlns="http://www.w3.org/2000/svg"
+                    fill="none"
+                    viewBox="0 0 24 24"
+                    strokeWidth={1.5}
+                    stroke="currentColor"
+                    className="size-5 pt-1 fill-[#AE859B] text-[#AE859B]"
+                  >
+                    <path
+                      strokeLinecap="round"
+                      strokeLinejoin="round"
+                      d="M11.48 3.499a.562.562 0 0 1 1.04 0l2.125 5.111a.563.563 0 0 0 .475.345l5.518.442c.499.04.701.663.321.988l-4.204 3.602a.563.563 0 0 0-.182.557l1.285 5.385a.562.562 0 0 1-.84.61l-4.725-2.885a.562.562 0 0 0-.586 0L6.982 20.54a.562.562 0 0 1-.84-.61l1.285-5.386a.562.562 0 0 0-.182-.557l-4.204-3.602a.562.562 0 0 1 .321-.988l5.518-.442a.563.563 0 0 0 .475-.345L11.48 3.5Z"
+                    />
+                  </svg>
+                  <p className="text-md pl-1 text-[#AE859B]">4.7</p>
+                </div>
+
+                <p className="w-2xs pt-4 pl-1">
+                  Special <span className="block">Chiken rice Bowl</span>{" "}
+                </p>
+                {/* Price */}
+              </div>
+              <div className="absolute flex justify-center items-center w-11 h-11 bottom-8 right-5 bg-[#AE859B] rounded-full text-sm text-white font-medium">
+                250
+              </div>
+          </div>
+          <div className="relative flex flex-1/4 h-50 bg-[#FFFFFF] rounded-3xl">
+            <div className="absolute top-[-50px] left-23 w-30 h-30 rounded-full bg-red-200"></div>
+              <div className="absolute w-30 h-38 bottom-0 p-4 pt-8">
+                {/* Rating */}
+                <div className="flex">
+                  <svg
+                    xmlns="http://www.w3.org/2000/svg"
+                    fill="none"
+                    viewBox="0 0 24 24"
+                    strokeWidth={1.5}
+                    stroke="currentColor"
+                    className="size-5 pt-1 fill-[#AE859B] text-[#AE859B]"
+                  >
+                    <path
+                      strokeLinecap="round"
+                      strokeLinejoin="round"
+                      d="M11.48 3.499a.562.562 0 0 1 1.04 0l2.125 5.111a.563.563 0 0 0 .475.345l5.518.442c.499.04.701.663.321.988l-4.204 3.602a.563.563 0 0 0-.182.557l1.285 5.385a.562.562 0 0 1-.84.61l-4.725-2.885a.562.562 0 0 0-.586 0L6.982 20.54a.562.562 0 0 1-.84-.61l1.285-5.386a.562.562 0 0 0-.182-.557l-4.204-3.602a.562.562 0 0 1 .321-.988l5.518-.442a.563.563 0 0 0 .475-.345L11.48 3.5Z"
+                    />
+                  </svg>
+                  <p className="text-md pl-1 text-[#AE859B]">4.7</p>
+                </div>
+
+                <p className="w-2xs pt-4 pl-1">
+                  Special <span className="block">Chiken rice Bowl</span>{" "}
+                </p>
+                {/* Price */}
+              </div>
+              <div className="absolute flex justify-center items-center w-11 h-11 bottom-8 right-5 bg-[#AE859B] rounded-full text-sm text-white font-medium">
+                250
+              </div>
+          </div>
+          <div className="relative flex flex-1/4 h-50 bg-[#FFFFFF] rounded-3xl">
+            <div className="absolute top-[-50px] left-23 w-30 h-30 rounded-full bg-red-200"></div>
+              <div className="absolute w-30 h-38 bottom-0 p-4 pt-8">
+                {/* Rating */}
+                <div className="flex">
+                  <svg
+                    xmlns="http://www.w3.org/2000/svg"
+                    fill="none"
+                    viewBox="0 0 24 24"
+                    strokeWidth={1.5}
+                    stroke="currentColor"
+                    className="size-5 pt-1 fill-[#AE859B] text-[#AE859B]"
+                  >
+                    <path
+                      strokeLinecap="round"
+                      strokeLinejoin="round"
+                      d="M11.48 3.499a.562.562 0 0 1 1.04 0l2.125 5.111a.563.563 0 0 0 .475.345l5.518.442c.499.04.701.663.321.988l-4.204 3.602a.563.563 0 0 0-.182.557l1.285 5.385a.562.562 0 0 1-.84.61l-4.725-2.885a.562.562 0 0 0-.586 0L6.982 20.54a.562.562 0 0 1-.84-.61l1.285-5.386a.562.562 0 0 0-.182-.557l-4.204-3.602a.562.562 0 0 1 .321-.988l5.518-.442a.563.563 0 0 0 .475-.345L11.48 3.5Z"
+                    />
+                  </svg>
+                  <p className="text-md pl-1 text-[#AE859B]">4.7</p>
+                </div>
+
+                <p className="w-2xs pt-4 pl-1">
+                  Special <span className="block">Chiken rice Bowl</span>{" "}
+                </p>
+                {/* Price */}
+              </div>
+              <div className="absolute flex justify-center items-center w-11 h-11 bottom-8 right-5 bg-[#AE859B] rounded-full text-sm text-white font-medium">
+                250
+              </div>
+          </div>
+
         </div>
       </div>
     </div>
