6c58fe71ac92a8f784c00d48267b2ab3260a4f3e
IT22342058 <IT22342058@my.sliit.lk>
2025-04-20 12:24:36 +0530

    commit

--- Frontend/Pages/Hasara/RestaurantDetails/RestaurantView.jsx
+++ Frontend/Pages/Hasara/RestaurantDetails/RestaurantView.jsx
@@ -2,6 +2,7 @@ import React, { useEffect, useState } from "react";
 import { useParams } from "react-router-dom";
 import Navbar from "../../../Components/Navbar";
 import Banner from "../../../Components/BannerRestaurant";
+import Footer from "../../../Components/Footer";
 
 const RestaurantDetails = () => {
   const { id } = useParams(); // restaurantId from URL
@@ -39,6 +40,7 @@ const RestaurantDetails = () => {
           ))}
         </div>
       )}
+      <Footer/>
     </div>
   );
 };
