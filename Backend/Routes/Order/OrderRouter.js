e6ee4b7548cf4db0483b5565123d4d4a0e8be365
prabashwara65 <prabashwara65@gmail.com>
2025-04-24 13:54:53 +0530

    commit

--- Backend/Routes/Order/OrderRouter.js
+++ Backend/Routes/Order/OrderRouter.js
@@ -1,2 +1,15 @@
-const express = express.Router()
+const express = require('express')
 
+const router = express.Router()
+
+const {
+    getAllOrders,
+    getOrderById,
+    PostOrder,
+} = require('../../Controller/Order/OrderController')
+
+router.get('/' , getAllOrders)
+
+router.get('/:id' , getOrderById)
+
+router.post('/' , PostOrder)
