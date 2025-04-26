a584ab33f6113f02ffa1f18cabef775bd259cc5f
prabashwara65 <prabashwara65@gmail.com>
2025-04-26 16:14:07 +0530

    commit

--- Frontend/Pages/Prabashwara/Orders/OrderHome.jsx
+++ Frontend/Pages/Prabashwara/Orders/OrderHome.jsx
@@ -3,6 +3,8 @@ import Navbar from "../../../Components/Navbar";
 
 const OrderHome = () => {
   const [orders, setOrders] = useState([]);
+  const [oneOrder, setOneOrder] = useState([])
+ 
 
   useEffect(() => {
     const fecthOrders = async () => {
@@ -19,24 +21,33 @@ const OrderHome = () => {
     console.log(`card has pressed  + ${id}`);
   };
 
+
+  useEffect(() => {
+    const getOneOrder = async (req , res) => {
+      const response = await fetch("http://localhost:4000/api/order/getOrders")
+      const data = response.json
+      setOneOrder(data)
+    }
+  })
+
   return (
-    <div className="container bg-teal-200 min-h-screen ">
+    <div className="container min-h-screen ">
       <Navbar />
       <div className="container flex flex-row gap-5 p-5 ">
-        <div className="overflow-auto bg-amber-300 w-1/3 h-[30rem] rounded-2xl">
+        <div className="overflow-auto bg-amber-300 w-1/3 h-[30rem] rounded-2xl p-4">
           {orders.map((order) => (
-            <div key={order._id} className="order-card">
+            <div key={order._id} className="p-4" onClick={() => handelClick(order._id)}>
               <h3>Order ID: {order._id}</h3>
               <p>Email: {order.email}</p>
-              <p>Total: ${order.totalAmount}</p>
-              <p>Status: {order.status}</p>
+              <p>Total: Rs {order.amount}</p>
+              {/* <p>Status: {order.status}</p> */}
               <p>Created At: {new Date(order.createdAt).toLocaleString()}</p>
 
               <h4>Items:</h4>
               <ul>
                 {order.items.map((item) => (
                   <li key={item.menuId}>
-                    {item.menuTitle} - Quantity: {item.quantity} - Price: $
+                    {item.menuTitle} - Quantity: {item.quantity} - Price: Rs 
                     {item.price}
                   </li>
                 ))}
@@ -45,7 +56,9 @@ const OrderHome = () => {
             </div>
           ))}
         </div>
-        <div className="bg-orange-300 w-2/3 ">fsdfs</div>
+        <div className="bg-orange-300 w-2/3 rounded-3xl">
+        
+        </div>
       </div>
     </div>
   );
