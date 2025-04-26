34f325e70caf48370ef67dbe821493a1d0c84862
prabashwara65 <prabashwara65@gmail.com>
2025-04-26 16:28:30 +0530

    commit

--- Frontend/src/App.jsx
+++ Frontend/src/App.jsx
@@ -39,7 +39,7 @@ function App() {
     <>
       <BrowserRouter>
         <Routes>
-          <Route path="/home" element={<Home />} />
+          <Route path="/" element={<Home />} />
           <Route path="/register" element={<Register />} />
           <Route path="/login" element={<Login />} />
           <Route path="/admin" element={<Adminpanel />} />
