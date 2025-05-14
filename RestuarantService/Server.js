61f0f57934919cf29f77fc281b620b59f147171c
prabashwara65 <prabashwara65@gmail.com>
2025-05-14 18:28:49 +0530

    added nodepoerts

--- RestuarantService/Server.js
+++ RestuarantService/Server.js
@@ -13,7 +13,7 @@ const app = express();
 // Middlewares
 app.use(express.json());
 app.use(cors({
-  origin: ["http://localhost:30080"],
+  origin: ["http://foodhub.local"],
   methods: ["GET", "POST", "PATCH", "DELETE", "PUT"],
   credentials: true,
 }));
